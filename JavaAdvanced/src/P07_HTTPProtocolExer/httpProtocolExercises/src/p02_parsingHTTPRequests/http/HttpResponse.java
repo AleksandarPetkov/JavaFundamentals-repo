5495669fabbc1d24a4ddf3df62acb5ca01c33264
AleksandarPetkov <A.PetkovPetkov@gmail.com>
2019-01-24 09:44:51 +0200

    HTTP Protocol

---
+++ JavaAdvanced/src/P07_HTTPProtocolExer/httpProtocolExercises/src/p02_parsingHTTPRequests/http/HttpResponse.java
@@ -0,0 +1,20 @@
+package p02_parsingHTTPRequests.http;
+
+import java.util.Map;
+
+public interface HttpResponse {
+
+    Map<String,String> getHeaders();
+
+    int getStatusCode();
+
+    String getContent();
+
+    byte[] getBytes(String decodedName);
+
+    void setStatusCode(int statusCode);
+
+    void setContent(String content);
+
+    void addHeader(String header, String value);
+}
